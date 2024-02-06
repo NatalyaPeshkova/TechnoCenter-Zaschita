d0412e692e28af8bc5fa8a97711d0bd404edd1ac
NatalyaPeshkova <fros.frost2010@yandex.ru>
2024-02-06 13:34:26 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// слайдер
+$('.sliders').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed:2000,
+    arrows:false,
+    speed:4000
+});
+
